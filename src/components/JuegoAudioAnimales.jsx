57a47302f98b3c22aed044d7ba71d7ce861ac690
Jonathan Vindas Benavides <jvindas@gmail.com>
2026-03-10 14:17:30 -0600

    Se agrega audios de los animales y se programa el boton de reproducir el audio

--- src/components/JuegoAudioAnimales.jsx
+++ src/components/JuegoAudioAnimales.jsx
@@ -1,19 +1,68 @@
-export default function JuegoAudioAnimales({ juego }) {
+import { AiTwotoneSound } from "react-icons/ai";
 
-    const play = ()=>{
-    const audio = new Audio(`/sonidos/${juego.sonido}`)
-    audio.play()
-    }
+export default function JuegoAudioAnimales({ juego,
+  elementos,
+  handleOnDragStart,
+  handleOnDrop,
+  handlePermitirDrop,
+  mensaje,
+  contadorJuegos,
+  juegos }) {
+
+   const handleReproducir = (elemento)=>{
+        const audio = new Audio(`/sonidos/${elemento.audio}`)
+        audio.play()
+   }
 
     return(
     <div>
 
-    <h2>{juego.titulo}</h2>
-    <h3>{juego.instrucciones}</h3>
+        <h2>{juego.titulo}</h2>
+        <h3>{juego.instrucciones}</h3>
 
-    <button onClick={play}>
-    🔊 Escuchar sonido
-    </button>
+         <div className="row">
+                {/* elementos para arrastrar */}
+                <div className="col-12 d-flex justify-content-center gap-3 mt-5 mb-5">
+                    {elementos.map(elemento =>(
+                        <div 
+                            className="d-flex flex-column justify-content-center align-items-center" 
+                            key={elemento.id} 
+                            draggable
+                            onDragStart={(e)=> handleOnDragStart(e, elemento)}
+                        >   
+                            <AiTwotoneSound className="icono-sonido" />
+                            <button 
+                                className="btn btn-outline-success"
+                                onClick={()=>handleReproducir(elemento)}
+                            >Reproducir</button>
+                        </div>
+                    ))}
+                </div>
+
+                {/* areas para dejar caer los elementos */}
+                <div className="col-12 d-flex justify-content-center gap-5 mt-5">
+                    {
+                        juego?.areas.map(juego=>(
+                            <div 
+                                key={juego.id} 
+                                className="areas"
+                                onDrop={(e)=> handleOnDrop(e, juego.id)}
+                                onDragOver={handlePermitirDrop}
+                            >
+                                <p>{juego.nombre}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+                
+
+                <h3>{mensaje}</h3>
+
+                <p>
+                    Juego {contadorJuegos + 1} de {juegos.length}
+                </p>
+
+            </div>
 
     </div>
 )
