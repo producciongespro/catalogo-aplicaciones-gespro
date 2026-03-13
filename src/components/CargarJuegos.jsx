8ea616cf768222e9f5c4ff30aa4cade8cf5cb502
Jonathan Vindas Benavides <jvindas@gmail.com>
2026-03-13 11:59:37 -0600

    Se agrega el juego de Mapa de Costa Rica, se crea el componente Mapa

--- src/components/CargarJuegos.jsx
+++ src/components/CargarJuegos.jsx
@@ -1,12 +1,14 @@
 import JuegoImgMusica from "./JuegoImgMusica"
 import JuegoAudioAnimales from "./JuegoAudioAnimales"
 import JuegoImgFormas from "./JuegoImgFormas"
+import JuegoMapaCr from "./JuegoMapaCr"
 
 export default function CargarJuegos(props) {
     const tipos = {
         imagenes: JuegoImgMusica,
         sonido: JuegoAudioAnimales,
-        imagenesFormas: JuegoImgFormas
+        imagenesFormas: JuegoImgFormas,
+        mapaCR: JuegoMapaCr
     }
 
     const Componente = tipos[props.juego.tipo]
---
+++ src/components/JuegoMapaCr.jsx
@@ -0,0 +1,55 @@
+import ProgresoJuego from "./ProgresoJuego";
+import TituloJuego from "./TituloJuegos";
+import MapaCostaRica from "./MapaCostaRica";
+
+export default function JuegoMapaCr({
+  juego,
+  elementos,
+  handleOnDragStart,
+  handleOnDrop,
+  handlePermitirDrop,
+  mensaje,
+  contadorJuegos,
+  juegos,
+  areaActiva,
+  estadoDrop,
+}) {
+  return (
+    <div className="pt-3">
+      <TituloJuego titulo={juego.titulo} instrucciones={juego.instrucciones} />
+
+      <div className="row">
+        {/* provincias arrastrables */}
+
+        <div className="col-12 d-flex justify-content-center flex-wrap gap-3 mt-5 mb-5">
+          {elementos.map((elemento) => (
+            <img
+              key={elemento.id}
+              src={`/imagenes/${elemento.imagen}`}
+              className="elemento"
+              draggable
+              onDragStart={(e) => handleOnDragStart(e, elemento)}
+            />
+          ))}
+        </div>
+
+        {/* mapa de cr en svg */}
+
+        <div className="col-12 d-flex justify-content-center mt-5">
+            <MapaCostaRica
+                handleOnDrop={handleOnDrop}
+                handlePermitirDrop={handlePermitirDrop}
+                areaActiva={areaActiva}
+                estadoDrop={estadoDrop}
+            />
+        </div>
+
+        <ProgresoJuego
+          mensaje={mensaje}
+          contadorJuegos={contadorJuegos}
+          juegos={juegos}
+        />
+      </div>
+    </div>
+  );
+}
