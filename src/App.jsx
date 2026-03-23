f5ce6ff3492007c14ec0f0282082ff220f6fc9bb
Jonathan Vindas Benavides <jvindas@gmail.com>
2026-03-23 08:59:21 -0600

    Se crea el data json para las tarjetas del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
+import JuegoMemoria from "./components/JuegoMemoria"
+
 
 
 function App() {
 
+  const [tarjetas, setTarjetas]= useState(null)
+  const [error, setError]= useState(null)
+
+  const setup = async ()=>{
+    try {
+      const res = await fetch("/data/data-tarjetas.json")
+      const data = res.json()
+      setTarjetas(data)
+      console.log(tarjetas);
+      
+    } catch (err) {
+      console.log(err);
+      setError("No se cargaron los datos de las tarjetas")
+    }
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <h1>Desde app juego memoria</h1>
+    <JuegoMemoria tarjetas={tarjetas}/>
   )
 }
 
---
+++ src/components/JuegoMemoria.jsx
@@ -0,0 +1,11 @@
+export default function JuegoMemoria({tarjetas}) {
+  console.log(tarjetas);
+  
+  return (
+    <div className="container text-center">
+      <h1 className="mt-3 text-primary">Juego de Memoria</h1>
+
+      <h4 className="mt-3">Nivel 1</h4>
+    </div>
+  );
+}
